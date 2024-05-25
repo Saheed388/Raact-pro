7d0d4cf7a70f8789338a299d716428ea4222c61b
saheed <saheedjimoh338@gmail.com>
2024-05-25 09:53:32 +0100

    new commit

---
+++ card-creation/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt={props.alt} />
+      <p>{props.tel}</p>
+      <p>{props.mail}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <div>
+    <h1>My Contacts</h1>
+    <Card
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      alt="image"
+      tel="+123 456 789"
+      mail="b@beyonce.com"
+    />
+
+    <Card
+      name="Jack Bauer"
+      img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      alt="image"
+      tel="+123 456 789"
+      mail="b@beyonce.com"
+    />
+  </div>,
+  document.getElementById("root")
+);
---
+++ exercise_code.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import "./styles.css"; // Make sure you have a styles.css file with .heading class
+
+const currentTime = new Date().getHours(); // Get the current hour
+
+let greeting;
+const customStyle = {
+  color: "",
+};
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Evening";
+  customStyle.color = "blue";
+}
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" style={customStyle}>
+      {greeting}
+    </h1>
+  </div>,
+  document.getElementById("root")
+);
