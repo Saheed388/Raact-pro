893c7f1e9433f3ba7affc02d562be428796d441d
saheed <saheedjimoh338@gmail.com>
2024-05-26 11:08:08 +0100

    new commit

--- first-keeper-project/App.jsx
+++ first-keeper-project/App.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import Header from "./Header";
-import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
-
-function createNote(notes) {
-  return <Note key={notes.id} title={notes.title} content={notes.content} />;
-}
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      {notes.map(createNote)}
+      <Note />
       <Footer />
-    </div>
+    </>
   );
 }
 
