a991077065560f747b617ee0e41edf94c207499b
saheed <saheedjimoh338@gmail.com>
2024-05-27 13:33:24 +0100

    new commit

---
+++ status-strike-unstrike/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  const [count, setCount] = React.useState(0);
+  // The count is the value userState, setCount is a function.
+  //which will be use to update the value in our state
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
+}
+export default App;
